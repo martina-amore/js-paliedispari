c7ad7a61e8895fd0b56b19ad7a3b51b384612a18
Martina Amore <martina.amore02@gmail.com>
2020-10-27 18:53:19 +0100

    Correzioni ad entrambi gli script, implementato nuovo script della parola palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,25 +1,75 @@
+// // PRIMO METODO: SPLIT-REVERSE-JOIN
+// // CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
+// var parola = prompt("Inserisci una parola");
+// var risultato;
+//
+// // CREO UNA FUNZIONE PER CAPIRE SE LA PAROLA E' PALINDROMA
+// function palindroma(word) {
+//     // DIVIDO LA PAROLA IN CARATTERI
+//     var splitParola = word.split("");
+//     // SISTEMO I CARATTERI AL CONTRARIO
+//     var reverseParola = splitParola.reverse();
+//     // UNISCO I CARATTERI AL CONTRARIO IN UN'UNICA PAROLA
+//     var joinParola = reverseParola.join("");
+//     // STABILISCO LA CONDIZIONE PER CUI, SE LA PAROLA INIZIALE
+//     // E' UGUALE ALLA PAROLA FORMATA DAI CARATTERI AL CONTRARIO, QUINDI SE E'
+//     // PALINDROMA, LA CONDIZIONE RISULTA VERA, ALTRIMENTI FALSA
+//     if (word == joinParola) {
+//         risultato = true;
+//     }
+//     else {
+//         risultato = false;
+//     }
+//     return risultato;
+// }
+//
+// // RICHIAMO LA FUNZIONE
+// palindroma(parola);
+//
+// // STABILISCO LA CONDIZIONE PER CUI, SE LA PAROLA E' PALINDROMA,
+// // STAMPO CHE LO E', ALTRIMENTI CHE NON LO E'
+// if (risultato == true) {
+//     document.getElementById("risultato").innerHTML = parola + " è una parola palindroma";
+// }
+//
+// else {
+//     document.getElementById("risultato").innerHTML = parola + " non è una parola palindroma";
+// }
+
+
+// SECONDO METODO: CICLO FOR
 // CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
-var parola = prompt("Inserisci una parola")
+var parola = prompt("Inserisci una parola");
 
 // CREO UNA FUNZIONE PER CAPIRE SE LA PAROLA E' PALINDROMA
-function palindroma() {
-    // DIVIDO LA PAROLA IN CARATTERI
-    var splitParola = parola.split("");
-    // SISTEMO I CARATTERI AL CONTRARIO
-    var reverseParola = splitParola.reverse();
-    // UNISCO I CARATTERI AL CONTRARIO IN UN'UNICA PAROLA
-    var joinParola = reverseParola.join("");
+function palindroma(word) {
+    // DEFINISCO LA VARIABILE REVERSE COME VUOTA
+    var reverseParola = "";
+    // INSTAURO UN CICLO FOR CHE VA AVANTI FINO A QUANDO NON ESAURISCO I CARATTERI DELLA PAROLA
+    for (var i = word.length - 1; i >= 0; i--){
+        reverseParola += word[i];
+    }
     // STABILISCO LA CONDIZIONE PER CUI, SE LA PAROLA INIZIALE
-    // E' UGUALE ALLA PAROLA FORMATA DAI CARATTERI AL CONTRARIO,
-    // VIENE STAMPATO UN MESSAGGIO CHE DICE CHE LA PAROLA E' PALINDROMA,
-    // ALTRIMENTI NON LO E'
-    if (parola == joinParola) {
-        document.getElementById("risultato").innerHTML = parola + " è una parola palindroma"
+    // E' UGUALE ALLA PAROLA FORMATA DAI CARATTERI AL CONTRARIO, QUINDI SE E'
+    // PALINDROMA, LA CONDIZIONE RISULTA VERA, ALTRIMENTI FALSA
+    if (word == reverseParola) {
+        risultato = true;
     }
     else {
-        document.getElementById("risultato").innerHTML = parola + " non è una parola palindroma"
+        risultato = false;
     }
+    return risultato;
 }
 
 // RICHIAMO LA FUNZIONE
-palindroma ();
+palindroma(parola);
+
+// STABILISCO LA CONDIZIONE PER CUI, SE LA PAROLA E' PALINDROMA,
+// STAMPO CHE LO E', ALTRIMENTI CHE NON LO E'
+if (risultato == true) {
+    document.getElementById("risultato").innerHTML = parola + " è una parola palindroma";
+}
+
+else {
+    document.getElementById("risultato").innerHTML = parola + " non è una parola palindroma";
+}
--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,9 +1,19 @@
 // CHIEDO ALL'UTENTE DI SCEGLIERE TRA PARI O DISPARI
-var podUtente = prompt("Scegli pari o dispari: scrivi 1 se scegli pari, 2 se scegli dispari");
+var podUtente = prompt("Scegli pari o dispari: scrivi 1 se scegli dispari, 2 se scegli pari");
 // SE L'UTENTE NON INSERISCE 1 O 2 PER SCEGLIERE, IL PROGRAMMA NON VA AVANTI E CONTINUA A CHIEDERE ALL'UTENTE DI INSERIRE 1 O 2
 while (podUtente < 1 || podUtente > 2) {
-    podUtente = parseInt(prompt("Hai inserito un numero non valido. Scrivi 1 se scegli pari, 2 se scegli dispari:"));
+    podUtente = parseInt(prompt("Hai inserito un numero non valido. Scrivi 1 se scegli dispari, 2 se scegli pari:"));
 }
+
+// SE L'UTENTE INSERISCE PARI, LA CONDIZIONE E' VERA, ALTRIMENTI E' FALSA
+var podIsEven;
+if (podUtente % 2 == 0) {
+    podIsEven = true;
+}
+else {
+    podIsEven = false;
+}
+
 // CHIEDO ALL'UTENTE DI SCEGLIERE UN NUMERO TRA 1 E 5
 var numeroUtente = parseInt(prompt("Inserisci un numero compreso tra 1 e 5:"));
 // SE L'UTENTE NON INSERISCE UN NUMERO TRA 1 E 5, IL PROGRAMMA NON VA AVANTI E CONTINUA A CHIEDERE ALL'UTENTE DI INSERIRE UN NUMERO TRA 1 E 5
@@ -21,29 +31,39 @@ function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// FACCIO LA SOMMA TRA IL NUMERO UTENTE E IL NUMERO CASUALE, RICHIAMANDO LA FUNZIONE, E LA STMAMPO
+// FACCIO LA SOMMA TRA IL NUMERO UTENTE E IL NUMERO CASUALE, RICHIAMANDO LA FUNZIONE, E LA STAMPO
 var somma = numeroUtente + numeroRandom(1, 5);
 document.getElementById("totale").innerHTML = "La somma del numero che hai scelto e del numero generato dal computer è " + somma;
 
 // FUNZIONE PER STABILIRE SE LA SOMMA E' PARI O DISPARI
-function pariODispari() {
-    // SE L'UTENTE HA SCELTO PARI E LA SOMMA E' PARI, STAMPO CHE L'UTENTE HA VINTO
-    if (somma % 2 == 0 && podUtente == 1) {
-        document.getElementById("finale-pod").innerHTML = "Hai scelto pari e la somma è pari, quindi hai vinto!";
+var sumIsEven;
+function pariODispari(val1) {
+    // SE LA SOMMA E' PARI, IL VALORE E' VERO, ALTRIMENTI FALSO
+    if (val1 % 2 == 0) {
+        sumIsEven = true;
     }
-    // SE L'UTENTE HA SCELTO DISPARI E LA SOMMA E' PARI, STAMPO CHE IL COMPUTER HA VINTO
-    else if (somma % 2 == 0 && podUtente == 2) {
-        document.getElementById("finale-pod").innerHTML = "Hai scelto dispari e la somma è pari, quindi ha vinto il computer";
-    }
-    // SE L'UTENTE HA SCELTO PARI E LA SOMMA E' DISPARI, STAMPO CHE IL COMPUTER HA VINTO
-    else if (somma % 2 == 1 && podUtente == 1) {
-        document.getElementById("finale-pod").innerHTML = "Hai scelto pari e la somma è dispari, quindi ha vinto il computer";
-    }
-    // SE L'UTENTE HA SCELTO DISPARI E LA SOMMA E' DISPARI, STAMPO CHE L'UTENTE HA VINTO
     else {
-        document.getElementById("finale-pod").innerHTML = "Hai scelto dispari e la somma è dispari, quindi hai vinto!";
+        sumIsEven = false;
     }
+    return sumIsEven;
 }
 
 // RICHIAMO LA FUNZIONE DELLA SOMMA PARI O DISPARI
-pariODispari();
+pariODispari(somma);
+
+// SE L'UTENTE HA SCELTO PARI E LA SOMMA E' PARI, STAMPO CHE L'UTENTE HA VINTO
+if (podIsEven == true && sumIsEven == true) {
+    document.getElementById("finale-pod").innerHTML = "Hai scelto pari e la somma è pari, quindi hai vinto!";
+}
+// SE L'UTENTE HA SCELTO DISPARI E LA SOMMA E' PARI, STAMPO CHE IL COMPUTER HA VINTO
+else if (podIsEven == false && sumIsEven == true) {
+    document.getElementById("finale-pod").innerHTML = "Hai scelto dispari e la somma è pari, quindi ha vinto il computer";
+}
+// SE L'UTENTE HA SCELTO PARI E LA SOMMA E' DISPARI, STAMPO CHE IL COMPUTER HA VINTO
+else if (podIsEven == true && sumIsEven == false) {
+    document.getElementById("finale-pod").innerHTML = "Hai scelto pari e la somma è dispari, quindi ha vinto il computer";
+}
+// SE L'UTENTE HA SCELTO DISPARI E LA SOMMA E' DISPARI, STAMPO CHE L'UTENTE HA VINTO
+else {
+    document.getElementById("finale-pod").innerHTML = "Hai scelto dispari e la somma è dispari, quindi hai vinto!";
+}
